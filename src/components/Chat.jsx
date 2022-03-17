4bda209adb7c41d4a0d1d6b013ae82a71e6ed394
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-03-17 13:08:08 -0500

    Chat component setMessages snapshot.map from firebase db

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SignOut } from './index'
+import { db } from '../firebase'
 
 const Chat = () => {
+
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        db.collection("messages").orderBy('createdAt').limit(50).onSnapshot((snapshot) => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+
   return (
     <div>
         <SignOut />
-        Chat
+        {messages.map(({id, text, photoURL}) => (
+            <div key={id}>
+                <img src={photoURL} alt="" />
+                <p>{text}</p>
+            </div>
+        ))}
     </div>
   )
 }
