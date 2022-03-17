31be966e1220fa47fda19ab62dee3506f2e44bdc
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-03-17 12:06:22 -0500

    initialilze firebase and add components SignIn & Chat

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const SignIn = () => {
+
+    const signInWithGoogle = () => {
+
+    }
+    
+  return (
+    <div>
+        <Button onClick={signInWithGoogle}>Sign In with Google</Button>
+    </div>
+  )
+}
+
+export default SignIn
